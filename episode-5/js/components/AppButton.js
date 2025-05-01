f65341d765799ffa6ce7fd6d942714b900259ff6
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-01 13:46:42 +0600

    wip: Complete custom components

--- episode-5/js/components/AppButton.js
+++ episode-5/js/components/AppButton.js
@@ -1,13 +1,27 @@
 export default {
-    template: `
-        <button class="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded disabled:cursor-not-allowed" :disabled="processing">
+  template: `
+        <button 
+        :class="{
+                'font-bold py-2 px-4 rounded': true,
+                'bg-blue-500 hover:bg-blue-600': type   === 'primary',
+                'bg-red-500 hover:bg-red-700': type     === 'danger',
+                'bg-green-500 hover:bg-green-700': type === 'success',
+                'bg-gray-200 hover:bg-gray-400 disabled:cursor-not-allowed': type   === 'muted',
+                'is-loading': processing
+                }" 
+                :disabled="processing">
             <slot />
         </button>
     `,
 
-    data () {
-        return {
-            processing: true
-        }
-    }
-}
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+    processing: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
