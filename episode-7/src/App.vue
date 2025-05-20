4b495476d015f983c2bfe944317fff56200ea16f
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-20 23:03:13 +0600

    wip: bind text

--- episode-7/src/App.vue
+++ episode-7/src/App.vue
@@ -1,8 +1,22 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      firstName: "Ashikur",
+      lastName : "Rahman",
+      company  : "Codeshaper"
+    }
+  }
+};
 </script>
 
 <template>
-  <div class="text-3xl font-bold underline">Hello Vue js.</div>
+  <div class="grid h-screen place-items-center">
+    <div class="font-bold text-gray-800">
+      <p class="text-3xl">{{ firstName }} {{ lastName }}</p>
+      <p>{{ company }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
