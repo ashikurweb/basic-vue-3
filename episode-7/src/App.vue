519059f801af4b6726d5f6f6a16f709c0db1a7ae
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-21 19:53:42 +0600

    wip: v-binding

--- episode-7/src/App.vue
+++ episode-7/src/App.vue
@@ -4,7 +4,10 @@ export default {
     return {
       firstName: "Ashikur",
       lastName : "Rahman",
-      company  : "Codeshaper"
+      company  : "Codeshaper",
+      position : "<h1>Junior Software Engineer</h1>",
+      button   : `<a href="#" onclick="alert('You have clicked!')" class="bg-blue-500 px-4 py-2 rounded text-white">Button</a>`,
+      headingId: 'heading',
     }
   }
 };
@@ -12,9 +15,12 @@ export default {
 
 <template>
   <div class="grid h-screen place-items-center">
-    <div class="font-bold text-gray-800">
+    <div class="font-bold text-center text-gray-800">
       <p class="text-3xl">{{ firstName }} {{ lastName }}</p>
       <p v-text="company"></p>
+      <p v-html="position" class="mt-2"></p>
+      <p v-html="button" class="mt-2"></p>
+      <h1 v-bind:id="headingId" class="mt-2">My Heading</h1>
     </div>
   </div>
 </template>
