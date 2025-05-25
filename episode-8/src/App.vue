15c546cfed4b1f72a327e3573a6053a53129a821
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-25 22:54:51 +0600

    wip: class binding

--- episode-8/src/App.vue
+++ episode-8/src/App.vue
@@ -1,15 +1,24 @@
 <script>
-  export default {
-    data() {
-      return {
-        name: "Ashikur Rahman"
-      }
+export default {
+  data() {
+    return {
+      msg: 'Hello World!',
+      headingColor: 'is-demo',
+      isPromoted: true
     }
   }
+}
 </script>
 
 <template>
-  <div class="grid place-items-center h-screen">
-    <h1>{{ name }}</h1>
-  </div>
+  <h1 v-bind:class="headingColor">{{ msg }}</h1>
+  <h2 :class="isPromoted ? 'text-amber-500 italic text-3xl hover:text-5xl' : 'text-green-500 text-3xl'">Promoted Movie</h2>
 </template>
+
+<style scoped>
+.is-demo {
+  color: red;
+  font-size: 4rem;
+}
+
+</style>
