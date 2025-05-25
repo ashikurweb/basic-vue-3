cd0c23cdff50f2ac67124bed326f8f6e00b14cb7
ashikurweb <ashikurrahman7194@gmail.com>
2025-05-25 19:41:43 +0600

    wip: episode 8 created

---
+++ episode-8/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    data() {
+      return {
+        name: "Ashikur Rahman"
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="grid place-items-center h-screen">
+    <h1>{{ name }}</h1>
+  </div>
+</template>
